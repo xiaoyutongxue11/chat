70098346bae452d1201610f9f4562b0a9583c6b1
tyy123 <1572235056@qq.com>
2024-10-09 21:33:34 +0800

    feat：每条消息的展示

--- client/src/components/MessageShow/index.tsx
+++ client/src/components/MessageShow/index.tsx
@@ -1,151 +1,142 @@
-import { Image, Modal } from 'antd';
-import { useEffect, useState } from 'react';
-
+import { IChatContentProps, IMediaInfo, IMessageShowProps } from './type';
 import styles from './index.module.less';
-import { IChatContentProps, IMessageShowProps, IMediaInfo } from './type';
-
-import { ChatImage, LoadErrorImage } from '@/assets/images';
-import ImageLoad from '@/components/ImageLoad';
-import { serverURL } from '@/config';
-import {
-	getMediaSize,
-	getMediaShowSize,
-	getFileName,
-	getFileIcons,
-	downloadFile,
-	urlExists
-} from '@/utils/file';
-import { userStorage } from '@/utils/storage';
 import { formatChatContentTime } from '@/utils/time';
+import { userStorage } from '@/utils/storage';
+import { useEffect, useState } from 'react';
+import { ChatImage, LoadErrorImage } from '@/assets/images';
+import {
+  downloadFile,
+  getFileIcons,
+  getFileName,
+  getMediaShowSize,
+  getMediaSize,
+  urlExists
+} from '@/utils/file';
+import { serverURL } from '@/config';
+import { Image, Modal } from 'antd';
+import ImageLoad from '../ImageLoad';
 
 const MessageShow = (props: IMessageShowProps) => {
-	const { showTime, message } = props;
-	const user = JSON.parse(userStorage.getItem());
-	const { sender_id, content, avatar, type, file_size, created_at } = message;
+  const { showTime, message } = props;
+  const { sender_id, content, avatar, type, file_size, created_at } = message;
+  const user = JSON.parse(userStorage.getItem());
 
-	// 图片/视频和文件被清理时的兜底显示
-	const ChatContentPocket = () => (
-		<div className={`${styles.content_delete} ${styles.content_file}`}>
-			<img src={LoadErrorImage.FILE_DELETE} draggable="false"></img>
-			<span>文件已过期或被清理</span>
-		</div>
-	);
+  const ChatContentPocket = () => (
+    <div className={`${styles.content_delete} ${styles.content_file}`}>
+      <img src={LoadErrorImage.FILE_DELETE} alt="" draggable="false" />
+      <span>文件已过期或被清理</span>
+    </div>
+  );
+  // 消息内容（文本、图片、视频、文件）
+  const ChatContent = (props: IChatContentProps): JSX.Element | null => {
+    const { messageType, messageContent, fileSize } = props;
+    // 文件是否存在
+    const [isFileExist, setIsFileExist] = useState<boolean>(true);
+    const [isVideoPlay, setIsVideoPlay] = useState<boolean>(false);
+    const [curMediaInfo, setCurMediaInfo] = useState<IMediaInfo | null>(null);
 
-	// 消息内容 (分为文本、图片、视频和文件)
-	const ChatContent = (props: IChatContentProps): JSX.Element | null => {
-		const { messageType, messageContent, fileSize } = props;
-		const [curMediaInfo, setCurMediaInfo] = useState<IMediaInfo | null>(null);
-		const [isVideoPlay, setIsVideoPlay] = useState<boolean>(false);
-		const [isFileExist, setIsFileExist] = useState<boolean>(true); // 使用 useState 来定义文件是否存在的状态，默认为 true
+    useEffect(() => {
+      if (messageType !== 'text') {
+        urlExists(`${serverURL}${messageContent}`).then(res => {
+          if (!res) {
+            setIsFileExist(res);
+          }
+        });
+      }
+      if (messageType === 'image' || messageType === 'video') {
+        const mediaURL = serverURL + messageContent;
+        getMediaSize(mediaURL, messageType)
+          .then(size => {
+            setCurMediaInfo({ type: messageType, url: mediaURL, size });
+          })
+          .catch();
+      }
+    }, [messageType, messageContent]);
+    // 打开视频播放窗口
+    const handleOpenVideo = () => {
+      setIsVideoPlay(true);
+    };
 
-		useEffect(() => {
-			// 非文本消息时，检查链接是否有效
-			if (messageType !== 'text') {
-				urlExists(`${serverURL}${messageContent}`).then(res => {
-					if (!res) {
-						setIsFileExist(res);
-					}
-				});
-			}
-			if (messageType === 'image' || messageType === 'video') {
-				const mediaURL = serverURL + messageContent;
-				getMediaSize(mediaURL, messageType)
-					.then(size => {
-						setCurMediaInfo({ type: messageType, url: mediaURL, size });
-					})
-					.catch(() => {
-						/* empty */
-					});
-			}
-		}, [messageType, messageContent]);
-
-		// 打开视频的播放窗口
-		const handleOpenVideo = () => {
-			setIsVideoPlay(true);
-		};
-
-		// 消息内容
-		if (!isFileExist) return <ChatContentPocket />;
-		switch (messageType) {
-			case 'text':
-				return <div className={styles.content_text}>{messageContent}</div>;
-			case 'image':
-				return curMediaInfo && curMediaInfo ? (
-					<Image
-						width={getMediaShowSize(curMediaInfo.size, 'image').width}
-						src={curMediaInfo.url}
-						rootClassName="content_image"
-					/>
-				) : null;
-			case 'video':
-				return curMediaInfo && curMediaInfo ? (
-					<div className={styles.content_video}>
-						<video
-							src={serverURL + messageContent}
-							muted
-							style={{
-								width: getMediaShowSize(curMediaInfo.size, 'video').width
-							}}
-						/>
-						<img src={ChatImage.PLAY} alt="" onClick={handleOpenVideo} draggable="false" />
-						<Modal
-							open={isVideoPlay}
-							footer={null}
-							title="视频"
-							onCancel={() => setIsVideoPlay(false)}
-							destroyOnClose
-							width={800}
-						>
-							<video src={serverURL + messageContent} muted controls autoPlay width={750} />
-						</Modal>
-					</div>
-				) : null;
-			case 'file':
-				return (
-					<div
-						className={styles.content_file}
-						onClick={() => {
-							downloadFile(`${serverURL}${messageContent}`);
-						}}
-					>
-						<div className={styles.content_file_name}>
-							<span>{getFileName(messageContent)}</span>
-							{fileSize && <span>{fileSize}</span>}
-						</div>
-						<div className={styles.content_file_img}>
-							<img src={getFileIcons(messageContent)} draggable="false"></img>
-						</div>
-					</div>
-				);
-			default:
-				return null;
-		}
-	};
-
-	return (
-		<>
-			{showTime && created_at && (
-				<div className={styles.chat_notice}>
-					<span>{formatChatContentTime(created_at)}</span>
-				</div>
-			)}
-			{sender_id === user.id ? (
-				<div className={`${styles.self} ${styles.chat_item_content}`}>
-					<ChatContent messageType={type} messageContent={content} fileSize={file_size} />
-					<div className={styles.avatar}>
-						<ImageLoad src={avatar} />
-					</div>
-				</div>
-			) : (
-				<div className={`${styles.other} ${styles.chat_item_content}`}>
-					<div className={styles.avatar}>
-						<ImageLoad src={avatar} />
-					</div>
-					<ChatContent messageType={type} messageContent={content} fileSize={file_size} />
-				</div>
-			)}
-		</>
-	);
+    // 消息内容
+    if (!isFileExist) return <ChatContentPocket />;
+    switch (messageType) {
+      case 'text':
+        return <div className={styles.content_text}>{messageContent}</div>;
+      case 'image':
+        return curMediaInfo && curMediaInfo ? (
+          <Image
+            width={getMediaShowSize(curMediaInfo.size, 'image').width}
+            src={curMediaInfo.url}
+          />
+        ) : null;
+      case 'video':
+        return curMediaInfo && curMediaInfo ? (
+          <div className={styles.content_video}>
+            <video
+              src={curMediaInfo.url}
+              muted
+              style={{
+                width: getMediaShowSize(curMediaInfo.size, 'video').width
+              }}
+            ></video>
+            <img src={ChatImage.PLAY} alt="" onClick={handleOpenVideo} draggable="false" />
+            <Modal
+              open={isVideoPlay}
+              footer={null}
+              title="视频"
+              onCancel={() => setIsVideoPlay(false)}
+              destroyOnClose
+              width={800}
+            >
+              <video src={curMediaInfo.url}></video>
+            </Modal>
+          </div>
+        ) : null;
+      case 'file':
+        return (
+          <div
+            className={styles.content_file}
+            onClick={() => {
+              downloadFile(`${serverURL}${messageContent}`);
+            }}
+          >
+            <div className={styles.content_file_name}>
+              <span>{getFileName(messageContent)}</span>
+              {fileSize && <span>{fileSize}</span>}
+            </div>
+            <div className={styles.content_file_img}>
+              <img src={getFileIcons(messageContent)} alt="" draggable="false" />
+            </div>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+  return (
+    <>
+      {showTime && created_at && (
+        <div className={styles.chat_notice}>
+          <span>{formatChatContentTime(created_at)}</span>
+        </div>
+      )}
+      {sender_id === user.id ? (
+        <div className={`${styles.self} ${styles.chat_item_content}`}>
+          <ChatContent messageType={type} messageContent={content} fileSize={file_size} />
+          <div className={styles.avatar}>
+            <ImageLoad src={avatar} />
+          </div>
+        </div>
+      ) : (
+        <div className={`${styles.other} ${styles.chat_item_content}`}>
+          <div className={styles.avatar}>
+            <ImageLoad src={avatar} />
+          </div>
+          <ChatContent messageType={type} messageContent={content} fileSize={file_size} />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default MessageShow;
--- client/src/components/MessageShow/type.ts
+++ client/src/components/MessageShow/type.ts
@@ -1,33 +1,37 @@
-import { MessageType } from '@/components/ChatTool/type';
+// 消息类型
+export type MessageType = 'text' | 'image' | 'video' | 'file';
 
 /**
- * 组件用到的其它类型定义
+ * 组件中用到的其他类型定义
  */
-// 给右侧显示消息项组件传递的参数类型
+// 显示消息组件需要的参数
 export interface IMessageShowProps {
-	showTime: boolean;
-	message: IMessageItem;
+  showTime: boolean;
+  message: IMessageItem;
 }
-// 右侧显示消息项的类型 —— 在client\src\pages\Chat\index.tsx中也被引用
+
+// 消息项的类型
 export interface IMessageItem {
-	sender_id: number;
-	receiver_id: number;
-	content: string;
-	room: string;
-	avatar: string;
-	type: MessageType;
-	file_size: string | null;
-	created_at: Date;
+  sender_id: number;
+  receiver_id: number;
+  content: string;
+  room: string;
+  avatar: string;
+  type: MessageType;
+  file_size: string | null;
+  created_at: Date;
 }
+
 // 给消息展示组件传递的参数类型
 export interface IChatContentProps {
-	messageType: MessageType;
-	messageContent: string;
-	fileSize?: string | null;
+  messageType: MessageType;
+  messageContent: string;
+  fileSize?: string | null;
 }
-// 图片/视频的信息（类型，URL，尺寸）
+
+// 图片/视频的信息
 export interface IMediaInfo {
-	type: 'image' | 'video';
-	url: string;
-	size: { width: number; height: number };
+  type: 'image' | 'video';
+  url: string;
+  size: { width: number; height: number };
 }
